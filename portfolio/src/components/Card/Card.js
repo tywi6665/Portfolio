7f80747ab4c4cbe3f4354a159bd78c906edafaa8
tywi <tyletcole@gmail.com>
2018-11-29 17:04:32 -0700

    changing card

--- portfolio/src/components/Card/Card.js
+++ portfolio/src/components/Card/Card.js
@@ -5,28 +5,34 @@ import Container from "../Container";
 class Card extends Component {
 
     state = {
-        thumbnails: ["https://images.unsplash.com/reserve/E5CwLOxQSFimIXJurfpq_IMG_6424%20(1).jpg?dpr=2&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb"]
+        thumbnails: ["https://images.unsplash.com/reserve/E5CwLOxQSFimIXJurfpq_IMG_6424%20(1).jpg?dpr=2&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb",'https://images.unsplash.com/photo-1464054313797-e27fb58e90a9?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=996&q=80']
     }
 
-    expand = event => {
-        event.preventDefault();
-        document.getElementById("wrapper").classList.toggle("open");
-        document.getElementById("content").classList.toggle("open");
-    }
+    // expand = event => {
+    //     event.preventDefault();
+    //     const wrapper = document.getElementsByClassName("wrapper");
+    //     console.log(wrapper);
+    //     wrapper[0].classList.toggle("open")
+    //     const content = document.getElementsByClassName("content");
+    //     console.log(content);
+    //     content[0].classList.toggle("open");
+    // }
 
     render() {
         return (
             <Container>
                 {this.state.thumbnails.map(thumbnail => (
-                    <div onClick={this.expand} className="wrapper" id="wrapper">
-                        <div className="content" id="content">
-                            <img src={thumbnail} className="img" />
+                    <div  className="wrapper">
+                        <div className="tile">
+                            <img src={thumbnail} />
                             <div className="text">
-                                <div className="line title">
-                                    <p>I've got a long way to go!</p>
-                                </div>
-                                <div className="line subtitle">
-                                    <p>I've got a long way to go!</p>
+                                <h1>Lorem ipsum.</h1>
+                                <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                                <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                                <div className="dots">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
                                 </div>
                             </div>
                         </div>
