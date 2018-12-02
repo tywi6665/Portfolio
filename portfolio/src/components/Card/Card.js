87f22dbe55f9f040168e4f7a4776a5b85d981844
tywi <tyletcole@gmail.com>
2018-12-02 12:21:05 -0700

    font awesome

--- portfolio/src/components/Card/Card.js
+++ portfolio/src/components/Card/Card.js
@@ -1,52 +1,56 @@
 import React, { Component, Fragment } from "react";
 import "./Card.scss";
-import Container from "../Container";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+library.add(fab, fas);
 
 class Card extends Component {
 
     state = {
-        thumbnails: ["https://images.unsplash.com/reserve/E5CwLOxQSFimIXJurfpq_IMG_6424%20(1).jpg?dpr=2&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb",
-                     'https://images.unsplash.com/photo-1464054313797-e27fb58e90a9?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=996&q=80',
-                     'https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80']
+        thumbnails: [{
+                url:"./images/photo1.jpg",
+                h1: "Project 1",
+                h2: "I've got a long way to go!"
+            }, {
+                url:"./images/photo2.jpg",
+                h1: "Project 2",
+                h2: "I've got a long way to go!"
+            }, {
+                url: "./images/photo3.jpg",
+                h1: "Project 3",
+                h2: "I've got a long way to go!"
+            }]
     }
 
     render() {
         return (
-            <div className="container">
+            // <div className="contain">
+            <Fragment>
                 {this.state.thumbnails.map(thumbnail => (
                     <div  className="wrapper">
                         <div className="tile">
-                            <img src={thumbnail} />
+                            <img src={thumbnail.url} alt={thumbnail.h1}/>
                             <div className="text">
-                                <h1>Lorem ipsum.</h1>
-                                <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                                <h1>{thumbnail.h1}</h1>
+                                <h2 className="animate-text">{thumbnail.h2}</h2>
                                 <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
                                 <div className="dots">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
+                                    <span><FontAwesomeIcon icon={["fab", "github"]} size="2x"/></span>
+                                    <span><FontAwesomeIcon icon={["fas", "code"]} size="2x"/></span>
+                                    {/* <span><FontAwesomeIcon icon={["fas", "spinner"]} size="xs" spin/></span> */}
                                 </div>
                             </div>
                         </div>
                     </div>
                 ))}
-            </div>
+            </Fragment>
+            // </div>
         )
     }
 
 
 }
-
-// const Card = () => (
-//     <div class="wrapper">
-//         <div class="content">
-//             <div class="img"></div>
-//             <div class="text">
-//                 <div class="line title"></div>
-//                 <div class="line subtitle"></div>
-//             </div>
-//         </div>
-//     </div>
-// );
 
 export default Card;
