8393bceb2d889c614eb2fa258075073dff546012
tywi <tyletcole@gmail.com>
2018-12-03 14:39:52 -0700

    refining cards

--- portfolio/src/components/Card/Card.js
+++ portfolio/src/components/Card/Card.js
@@ -11,8 +11,11 @@ class Card extends Component {
     state = {
         thumbnails: [{
                 url:"./images/photo1.jpg",
-                h1: "Project 1",
-                h2: "I've got a long way to go!"
+                h1: "Color Wheel",
+                h2: "I've got a long way to go!",
+                icons1: ["fab", "react"],
+                icons2: ["fab", "node"],
+                icons3: ["fab", "aws"]
             }, {
                 url:"./images/photo2.jpg",
                 h1: "Project 2",
@@ -36,6 +39,12 @@ class Card extends Component {
                                 <h1>{thumbnail.h1}</h1>
                                 <h2 className="animate-text">{thumbnail.h2}</h2>
                                 <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                                <div className="animate-icons">
+                                    <h3>Technologies Used:</h3>
+                                    <span><FontAwesomeIcon icon={thumbnail.icons1} size="4x"/></span>
+                                    <span><FontAwesomeIcon icon={thumbnail.icons2} size="4x"/></span>
+                                    <span><FontAwesomeIcon icon={thumbnail.icons3} size="4x"/></span>
+                                </div>
                                 <div className="dots">
                                     <span><FontAwesomeIcon icon={["fab", "github"]} size="2x"/></span>
                                     <span><FontAwesomeIcon icon={["fas", "code"]} size="2x"/></span>
