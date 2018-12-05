7b15d1f71b0a76c74734ea7e3511d4b9f476de9a
tywi <tyletcole@gmail.com>
2018-12-05 14:33:39 -0700

    color palette

--- portfolio/src/components/Nav/Nav.js
+++ portfolio/src/components/Nav/Nav.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import "./Nav.scss";
+library.add(fab, fas);
+
 
 const Nav = () => (
     <nav>
         <ul>
-            <li><Link to={"/main"}>About</Link></li>
+            <li><Link to={"/main"}>About
+                    {/* <span><FontAwesomeIcon icon={["fas", "code"]} size="4x"/></span> */}
+                </Link>
+            </li>
             <li><Link to={"/projects"}>Projects</Link></li>
             <li><Link to={"/contact"}>Contact Me</Link></li>
         </ul>
