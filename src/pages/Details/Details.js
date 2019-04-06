b89d7f79ae45801333945226c6c63eddafe9d469
tywi6665 <tyletcole@gmail.com>
2019-04-06 15:02:33 -0600

    worked on details page

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -9,35 +9,44 @@ class Details extends Component {
         deets: [{
             id: 1,
             img: "./images/react-logo.png",
-            heading: "Portfolio Deets!!",
-            text: "Portfolio Deets!!"
+            heading: "Built With React.js",
+            text1: "React.js is a JavaScript library for creating dynamic, single page, and modularized web applications",
+            text2: "To take full advantage of React, I created many reuseable stateless and stateful components. Calling these premade components in various places allowed me to make my portfolio lightweight and cohesive",
+            text3: "I also utilized the react-router-dom npm package to simulate dynamic url addresses, and backend routing"
         },
         {
             id: 2,
             img: "./images/d3.svg",
-            heading: "Portfolio Deets!!",
-            text: "Portfolio Deets!!"
+            heading: "Animated With D3.js",
+            text1: "D3.js is a JavaScript library for creating custom animations and data visualiztions",
+            text2: "Portfolio Deets!!",
+            text3: "Portfolio Deets!!"
         },
         {
             id: 3,
             img: "./images/sass-logo.png",
-            heading: "Portfolio Deets!!",
-            text: "Portfolio Deets!!"
+            heading: "Styled With Sass",
+            text1: "Sass is a CSS preprocessor that allows for JavaScript-like styling functionality",
+            text2: "Portfolio Deets!!",
+            text3: "Portfolio Deets!!"
         }]
     }
 
     render() {
         return (
             <Container>
-                <h2>Details Page</h2>
+                <h2 className="detailsTitle">Details Page</h2>
                 <div className="detailsBox">
                     {this.state.deets.map(deet => (
                         <div className="deet" key={deet.id}>
                             <img className="deetImage" src={deet.img} />
+                            <h3>{deet.heading}</h3>
                             <ul>
-                                <li>{deet.text}</li>
-                                <li>{deet.text}</li>
-                                <li>{deet.text}</li>
+                                <li>{deet.text1}</li>
+                                <li>//</li>
+                                <li>{deet.text2}</li>
+                                <li>//</li>
+                                <li>{deet.text3}</li>
                             </ul>
                         </div>
                     ))}
