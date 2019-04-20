82521e1e4cd35858256d6eca991e02a2d7095455
tywi6665 <tyletcole@gmail.com>
2019-04-20 16:58:00 -0600

    added arcs to splash screen

--- src/components/withSplashScreen/withSplashScreen.js
+++ src/components/withSplashScreen/withSplashScreen.js
@@ -1,20 +1,21 @@
 import React, { Component } from "react";
+import * as d3 from "d3";
 import "./withSplashScreen.scss";
 
 function LoadingDots() {
     return (
         <div className="splashScreen">
             <div className="loading">
-                {/* <div className="growDot"></div> */}
+                <div id="canvas"></div>
                 <div className="loadingDots">
                     <div className="splashDot1">T</div>
                     <div className="splashDot2">Y</div>
                     <div className="splashDot3">W</div>
                     <div className="splashDot4">I</div>
                 </div>
-                <div className="loadingText">
+                {/* <div className="loadingText">
                     Awesomeness is on its way!!!
-                </div>
+                </div> */}
             </div>
         </div>
     );
@@ -30,12 +31,64 @@ function withSplashScreen(WrappedComponent) {
         };
 
         componentDidMount() {
+            this.movingArcs();
             setTimeout(() => {
                 this.setState({
                     loading: false
                 });
-            }, 3200)
+            }, 4500)
         };
+
+        movingArcs() {
+            // d3.select(".d3splash").remove();
+            
+            var height = 500,
+                width = 500,
+                twoPi = 2 * Math.PI,
+                formatPercent = d3.format(".0%"),
+                curvePercent = [0.1, 0.25, 0.35, 0.50, 0.75, 1.0, 0.80, 0.50, 0.35, 0.25, 0.1];
+
+            var color = d3.scaleSequential(d3.interpolatePuRd)
+            var colorValue = function (d, i) { return d; };
+      
+            var svg = d3.select("#canvas")
+                .append("svg")
+                .attr("height", `${height}px`)
+                .attr("width", `${width}px`)
+
+            var pathArc = d3.arc()
+                .innerRadius(width / 4)
+                .outerRadius(width / 4 + 5)
+
+            var curves = svg.append("g")
+                .attr("transform", `translate(${width / 2}, ${height / 2})`);
+
+            curves.selectAll("path")
+                .data(curvePercent)
+                .enter()
+                .append("path")
+                .each(arcs)
+                .style("fill", d => color(colorValue(d)))
+
+            function arcs(d, i) {
+                return d3.select(this)
+                    .transition()
+                    .ease(d3.easeBounce)
+                    .duration(1500)
+                    .attr("class", "arc")
+                    .attr("d", pathArc.startAngle(Math.random(twoPi)).endAngle(Math.random(twoPi) + twoPi * curvePercent[i]))
+                    .attr("transform", "scale(1." + i + ")")
+                    .on("end", animateArcs)
+            }
+
+            function animateArcs() {
+                return d3.selectAll(".arc")
+                    .transition()
+                    .ease(d3.easeExp)
+                    .duration(3000)
+                    .attr("transform", `translate(0, 0) rotate(${90})`)
+            }
+        }
 
         render() {
             if (this.state.loading) return LoadingDots();
